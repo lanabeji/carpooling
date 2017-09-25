7a47afd3cfc91ca9686246e9af6defc4042ae659
Camilo Alfonso <ca.alfonso967@uniandes.edu.co>
2017-09-25 11:06:29 -0500

    Adding

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,15 @@ var url = "mongodb://lanabeji:uWCI4oYSRS3duSy5@cluster0-shard-00-00-fwq0s.mongod
 
 function getUsuarios(callback) {
 
-    mongodb.connect(url, function (err,db){
-        if(err) throw err;
+    mongodb.connect(url, function (err, db) {
+        if (err) throw err;
         var usuarios = db.collection("usuarios");
 
-        usuarios.find({}).toArray(function (err2,usuarios){
-            if(err2) throw err2;
 
-        callback(usuarios);
+        usuarios.find({}).toArray(function (err2, usuarios) {
+            if (err2) throw err2;
+
+            callback(usuarios);
         });
     });
 }
@@ -21,10 +22,10 @@ function getUsuarios(callback) {
 
 function createUsuario(nuevo) {
 
-    mongodb.connect(url, function(err, db) {
+    mongodb.connect(url, function (err, db) {
         if (err) throw err;
 
-        db.collection("usuarios").insertOne(nuevo, function(err, res) {
+        db.collection("usuarios").insertOne(nuevo, function (err, res) {
             if (err) throw err;
             console.log("1 document inserted");
             db.close();
@@ -34,10 +35,10 @@ function createUsuario(nuevo) {
 
 function updateUsuario(myquery, newvalues) {
 
-    mongodb.connect(url, function(err, db) {
+    mongodb.connect(url, function (err, db) {
         if (err) throw err;
 
-        db.collection("usuarios").updateMany(myquery, newvalues, function(err, res) {
+        db.collection("usuarios").updateMany(myquery, newvalues, function (err, res) {
             if (err) throw err;
             console.log(res.result.nModified + " document(s) updated");
             db.close();
@@ -46,10 +47,10 @@ function updateUsuario(myquery, newvalues) {
 };
 
 function deleteUsuario(myquery) {
-    mongodb.connect(url, function(err, db) {
+    mongodb.connect(url, function (err, db) {
         if (err) throw err;
 
-        db.collection("usuarios").deleteOne(myquery, function(err, obj) {
+        db.collection("usuarios").deleteOne(myquery, function (err, obj) {
             if (err) throw err;
             console.log("1 document deleted");
             db.close();
@@ -59,15 +60,93 @@ function deleteUsuario(myquery) {
 
 
 //Saca todos los viajes de la coleccion viajes
-function getViajes(callback){
+function getViajes(callback) {
+
+
+    var nuevo1 = {
+        origen: "Uniandes",
+        destino: "Cajicá",
+        horaEncuentro: "2:00 p.m",
+        fecha: "27/Sep/2017",
+        valor: 5000,
+        cupos: 3,
+        pasajeros: [],
+        conductor: "Camilo Alfonso"
+    };
+    var nuevo2 = {
+        origen: "Zipaquirá",
+        destino: "Cajicá",
+        horaEncuentro: "5:00 p.m",
+        fecha: "28/Sep/2017",
+        valor: 2000,
+        cupos: 3,
+        pasajeros: [],
+        conductor: "Laura Bello"
+    };
+    var nuevo3 = {
+        origen: "Chía",
+        destino: "Cajicá",
+        horaEncuentro: "7:00 a.m",
+        fecha: "29/Sep/2017",
+        valor: 2000,
+        cupos: 3,
+        pasajeros: [],
+        conductor: "Andrés Gomez"
+    };
+    var nuevo4 = {
+        origen: "Cajicá",
+        destino: "Uniandes",
+        horaEncuentro: "5:00 a.m",
+        fecha: "30/Sep/2017",
+        valor: 5000,
+        cupos: 3,
+        pasajeros: [],
+        conductor: "Felipe Gutierrez"
+    };
+    var nuevo5 = {
+        origen: "Chía",
+        destino: "Zipaquirá",
+        horaEncuentro: "7:00 p.m",
+        fecha: "01/Oct/2017",
+        valor: 3000,
+        cupos: 3,
+        pasajeros: [],
+        conductor: "Alvaro Dueñas"
+    };
+
     mongodb.connect(url, function (err, db) {
-        if(err) throw err;
+        if (err) throw err;
 
         console.log("Get viajes function");
         var viajes = db.collection("viajes");
 
+        viajes.insertOne(nuevo1, function (err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+        });
+
+        viajes.insertOne(nuevo2, function (err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+        });
+
+        viajes.insertOne(nuevo3, function (err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+        });
+
+        viajes.insertOne(nuevo4, function (err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+        });
+
+        viajes.insertOne(nuevo5, function (err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+        });
+
         viajes.find({}).toArray(function (mongoError, viajes) {
-            if(mongoError) throw mongoError;
+            if (mongoError) throw mongoError;
 
             console.log(viajes.length);
             callback(viajes);
@@ -93,222 +172,222 @@ function login(usuario) {
         });
     });
 }
+
 //Lo mismo de usuario pero con viaje
-    function createViaje(viaje) {
+function createViaje(viaje) {
 
-        mongodb.connect(url, function (err, db) {
+    mongodb.connect(url, function (err, db) {
+        if (err) throw err;
+
+        db.collection("viajes").insertOne(viaje, function (err, res) {
             if (err) throw err;
-
-            db.collection("viajes").insertOne(viaje, function (err, res) {
-                if (err) throw err;
-                console.log("1 document inserted");
-                db.close();
-            });
+            console.log("1 document inserted");
+            db.close();
         });
-    };
+    });
+};
 
-    function updateViaje(myquery, newvalues) {
+function updateViaje(myquery, newvalues) {
 
-        mongodb.connect(url, function (err, db) {
+    mongodb.connect(url, function (err, db) {
+        if (err) throw err;
+
+        db.collection("viajes").updateMany(myquery, newvalues, function (err, res) {
             if (err) throw err;
-
-            db.collection("viajes").updateMany(myquery, newvalues, function (err, res) {
-                if (err) throw err;
-                console.log(res.result.nModified + " document(s) updated");
-                db.close();
-            });
+            console.log(res.result.nModified + " document(s) updated");
+            db.close();
         });
-    };
+    });
+};
 
 //Lo mismo de usuario pero con viaje, se borra un viaje dados los valores del viaje excepto pasajeros.
-    function deleteViaje(viaje) {
-        mongodb.connect(url, function (err, db) {
+function deleteViaje(viaje) {
+    mongodb.connect(url, function (err, db) {
+        if (err) throw err;
+
+        db.collection("viajes").deleteOne(viaje, function (err, obj) {
             if (err) throw err;
-
-            db.collection("viajes").deleteOne(viaje, function (err, obj) {
-                if (err) throw err;
-                console.log("1 document deleted");
-                db.close();
-            });
+            console.log("1 document deleted");
+            db.close();
         });
-    };
+    });
+};
 
-    function dropTodo() {
-        mongodb.connect(url, function (err, db) {
+function dropTodo() {
+    mongodb.connect(url, function (err, db) {
+        if (err) throw err;
+        db.dropCollection("usuarios", function (err, delOK) {
             if (err) throw err;
-            db.dropCollection("usuarios", function (err, delOK) {
-                if (err) throw err;
-                if (delOK) console.log("Collection usuarios deleted");
-            });
-            db.dropCollection("viajes", function (err, delOK) {
-                if (err) throw err;
-                if (delOK) console.log("Collection viajes deleted");
-            });
+            if (delOK) console.log("Collection usuarios deleted");
         });
-    };
-
-
-    router.post("/login", function (req, res) {
-        var usern = req.body.username;
-        var pass = req.body.password;
-
-        var usuario = {username: usern, password: pass};
-        login(usuario);
-        console.log("Usuario login index.js");
-    });
-
-    router.get("/getUsuarios", function (req, res) {
-        getUsuarios(function (usuarios) {
-            console.log("Index.js en getUsuarios");
-            res.json(usuarios);
+        db.dropCollection("viajes", function (err, delOK) {
+            if (err) throw err;
+            if (delOK) console.log("Collection viajes deleted");
         });
     });
+};
 
-    router.post("/createUsuario", function (req, res) {
 
-        var usernam = req.body.username;
-        var pass = req.body.password;
-        var nam = req.body.name;
-        var tip = req.body.tipo;
-        var pic = req.body.profile_pic;
-        var pla = req.body.placa;
-        var fot = req.body.foto;
+router.post("/login", function (req, res) {
+    var usern = req.body.username;
+    var pass = req.body.password;
 
-        var nuevo = {name: nam, username: usernam, password: pass, tipo: tip, profile_pic: pic, placa: pla, foto: fot};
+    var usuario = {username: usern, password: pass};
+    login(usuario);
+    console.log("Usuario login index.js");
+});
 
-        createUsuario(nuevo);
+router.get("/getUsuarios", function (req, res) {
+    getUsuarios(function (usuarios) {
+        console.log("Index.js en getUsuarios");
+        res.json(usuarios);
     });
+});
 
-    router.post("/updateUsuario", function (req, res) {
+router.post("/createUsuario", function (req, res) {
 
-        var usern = req.body.username;
-        var pass = req.body.password;
+    var usernam = req.body.username;
+    var pass = req.body.password;
+    var nam = req.body.name;
+    var tip = req.body.tipo;
+    var pic = req.body.profile_pic;
+    var pla = req.body.placa;
+    var fot = req.body.foto;
 
-        var nom = req.body.name;
-        var pic = req.body.profile_pic;
-        var pla = req.body.placa;
-        var fot = req.body.foto;
+    var nuevo = {name: nam, username: usernam, password: pass, tipo: tip, profile_pic: pic, placa: pla, foto: fot};
 
-        var myquery = {username: usern, password: pass};
-        var newvalues = {$set: {name: nom, profile_pic: pic, placa: pla, foto: fot}};
-        updateUsuario(myquery, newvalues);
-    });
+    createUsuario(nuevo);
+});
 
-    router.get("/deleteUsuario", function (req, res) {
-        var q = req.body.username;
-        var p = req.body.password;
+router.post("/updateUsuario", function (req, res) {
 
-        var query = {username: q, password: p};
+    var usern = req.body.username;
+    var pass = req.body.password;
 
-        deleteUsuario(query);
-    });
+    var nom = req.body.name;
+    var pic = req.body.profile_pic;
+    var pla = req.body.placa;
+    var fot = req.body.foto;
+
+    var myquery = {username: usern, password: pass};
+    var newvalues = {$set: {name: nom, profile_pic: pic, placa: pla, foto: fot}};
+    updateUsuario(myquery, newvalues);
+});
+
+router.get("/deleteUsuario", function (req, res) {
+    var q = req.body.username;
+    var p = req.body.password;
+
+    var query = {username: q, password: p};
+
+    deleteUsuario(query);
+});
 
 //Obtiene todos los viajes
-    router.get("/getViajes", function (req, res) {
+router.get("/getViajes", function (req, res) {
 
-        getViajes(function (viajes) {
-            res.json(viajes);
-        });
+    getViajes(function (viajes) {
+        res.json(viajes);
     });
+});
 
 //Crea un viaje con los valores dados por parámetro
-    router.post("/createViaje", function (req, res) {
+router.post("/createViaje", function (req, res) {
 
-        var ori = req.body.origen;
-        var dest = req.body.destino;
-        var hora = req.body.horaEncuentro;
-        var fech = req.body.fecha;
-        var val = req.body.valor;
-        var cup = req.body.cupos;
-        var pas = req.body.pasajeros;
-        var cond = req.body.conductor;
-        var fot = req.body.foto;
-        var pla = req.body.placa;
+    var ori = req.body.origen;
+    var dest = req.body.destino;
+    var hora = req.body.horaEncuentro;
+    var fech = req.body.fecha;
+    var val = req.body.valor;
+    var cup = req.body.cupos;
+    var pas = req.body.pasajeros;
+    var cond = req.body.conductor;
+    var fot = req.body.foto;
+    var pla = req.body.placa;
 
-        var viaje = {
+    var viaje = {
+        origen: ori,
+        destino: dest,
+        horaEncuentro: hora,
+        fecha: fech,
+        valor: val,
+        cupos: cup,
+        pasajeros: pas,
+        conductor: cond,
+        foto: fot,
+        placa: pla
+    };
+
+    createViaje(viaje);
+});
+
+router.post("/updateViaje", function (req, res) {
+
+    var ori = req.body.origen;
+    var dest = req.body.destino;
+    var hora = req.body.horaEncuentro;
+    var fech = req.body.fecha;
+    var val = req.body.valor;
+    var cup = req.body.cupos;
+    var pas = req.body.pasajeros;
+    var cond = req.body.conductor;
+    var fot = req.body.foto;
+    var pla = req.body.placa;
+
+    var myquery = {conductor: cond, fecha: fech};
+    var newvalues = {
+        $set: {
             origen: ori,
             destino: dest,
             horaEncuentro: hora,
-            fecha: fech,
             valor: val,
             cupos: cup,
             pasajeros: pas,
-            conductor: cond,
             foto: fot,
             placa: pla
-        };
-
-        createViaje(viaje);
-    });
-
-    router.post("/updateViaje", function (req, res) {
-
-        var ori = req.body.origen;
-        var dest = req.body.destino;
-        var hora = req.body.horaEncuentro;
-        var fech = req.body.fecha;
-        var val = req.body.valor;
-        var cup = req.body.cupos;
-        var pas = req.body.pasajeros;
-        var cond = req.body.conductor;
-        var fot = req.body.foto;
-        var pla = req.body.placa;
-
-        var myquery = {conductor: cond, fecha: fech};
-        var newvalues = {
-            $set: {
-                origen: ori,
-                destino: dest,
-                horaEncuentro: hora,
-                valor: val,
-                cupos: cup,
-                pasajeros: pas,
-                foto: fot,
-                placa: pla
-            }
-        };
-        updateViaje(myquery, newvalues);
-    });
+        }
+    };
+    updateViaje(myquery, newvalues);
+});
 
 //Borra un viaje con todos los parametros excepto pasajeros, no son necesarios.
-    router.get("/deleteViaje", function (req, res) {
+router.get("/deleteViaje", function (req, res) {
 
-        var ori = req.body.origen;
-        var dest = req.body.destino;
-        var hora = req.body.horaEncuentro;
-        var fech = req.body.fecha;
-        var val = req.body.valor;
-        var cup = req.body.cupos;
+    var ori = req.body.origen;
+    var dest = req.body.destino;
+    var hora = req.body.horaEncuentro;
+    var fech = req.body.fecha;
+    var val = req.body.valor;
+    var cup = req.body.cupos;
 
-        var cond = req.body.conductor;
-        var fot = req.body.foto;
-        var pla = req.body.placa;
+    var cond = req.body.conductor;
+    var fot = req.body.foto;
+    var pla = req.body.placa;
 
-        var query = {
-            origen: ori,
-            destino: dest,
-            horaEncuentro: hora,
-            fecha: fech,
-            valor: val,
-            cupos: cup,
-            conductor: cond,
-            foto: fot,
-            placa: pla
-        };
+    var query = {
+        origen: ori,
+        destino: dest,
+        horaEncuentro: hora,
+        fecha: fech,
+        valor: val,
+        cupos: cup,
+        conductor: cond,
+        foto: fot,
+        placa: pla
+    };
 
-        deleteViaje(query);
-    });
+    deleteViaje(query);
+});
 
 
 //Borra lo de la tabla por si escribo valores mal y quiero borrar
-    router.get("/dropTodo", function (req, res) {
-        dropTodo();
-    });
-
-router.get("/", function (req,res) {
-    res.render('index', { title: 'Express' });
+router.get("/dropTodo", function (req, res) {
+    dropTodo();
 });
 
+router.get("/", function (req, res) {
+    res.render('index', {title: 'Express'});
+});
 
 
 module.exports = router;
